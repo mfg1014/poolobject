8b4df660c1e84e1c5dc8dab068f0decd2c4f2dd3
mfg1014 <mfg1014@alu.ubu.es>
2023-02-24 17:50:04 +0100

    test cliente

---
+++ src/test/ubu/gii/dass/test/c01/ClientTest.java
@@ -0,0 +1,42 @@
+package ubu.gii.dass.test.c01;
+
+import static org.junit.Assert.*;
+
+import ubu.gii.dass.c01.Client;
+
+
+public class ReusablePoolTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.Client#main()}.
+	 */
+	@Test
+	public void testmain() {
+        Client c = new Client();
+        c.main();
+        assertNotEquals(c.pool, null);
+        assertNotEquals(c.r1, null);
+        assertNotEquals(c.r2, null);
+        assertNotEquals(c.r3, null);
+        assertNotEquals(c.logger, null);
+        
+		
+	}
+
+	
+
+}
